ddf80236448dd6851e615a630f1dcbd53ef23b9a
Sanqiuzhiji <2772179523@qq.com>
2024-06-16 12:41:51 +0800

    1

--- Core/Hardware_Inc/TFT.h
+++ Core/Hardware_Inc/TFT.h
@@ -39,30 +39,30 @@ extern uint16_t  BACK_COLOR; //背景颜色.默认为白色
 //以下IO定义直接操作寄存器，快速IO操作，刷屏速率可以达到28帧每秒！ 
 
 //GPIO置位（拉高）
-#define	LCD_CS_SET  GPIO_TYPE->BSRR=1<<LCD_CS    //片选端口  	PB11
-#define	LCD_RS_SET	GPIO_TYPE->BSRR=1<<LCD_RS    //数据/命令  PB10	  
-#define	LCD_RST_SET	GPIO_TYPE->BSRR=1<<LCD_RST   //复位			  PB12
+#define	LCD_CS_SET  HAL_GPIO_WritePin(TFT_CS_GPIO_Port,TFT_CS_Pin,GPIO_PIN_SET)   //片选端口  	
+#define	LCD_RS_SET	HAL_GPIO_WritePin(TFT_DC_GPIO_Port,TFT_DC_Pin,GPIO_PIN_SET)    //数据/命令  
+#define	LCD_RST_SET	HAL_GPIO_WritePin(TFT_Reset_GPIO_Port,TFT_Reset_Pin,GPIO_PIN_SET)   //复位	 
 
 //GPIO复位（拉低）							    
-#define	LCD_CS_CLR  GPIO_TYPE->BRR=1<<LCD_CS     //片选端口  	PB11
-#define	LCD_RS_CLR	GPIO_TYPE->BRR=1<<LCD_RS     //数据/命令  PB10	 
-#define	LCD_RST_CLR	GPIO_TYPE->BRR=1<<LCD_RST    //复位			  PB12
+#define	LCD_CS_CLR  HAL_GPIO_WritePin(TFT_CS_GPIO_Port,TFT_CS_Pin,GPIO_PIN_RESET)      //片选端口  
+#define	LCD_RS_CLR	HAL_GPIO_WritePin(TFT_DC_GPIO_Port,TFT_DC_Pin,GPIO_PIN_RESET)      //数据/命令   
+#define	LCD_RST_CLR	HAL_GPIO_WritePin(TFT_Reset_GPIO_Port,TFT_Reset_Pin,GPIO_PIN_RESET)  //复位			 
 
 //画笔颜色
 #define WHITE       0xFFFF
 #define BLACK      	0x0000	  
 #define BLUE       	0x001F  
 #define BRED        0XF81F
-#define GRED 			 	0XFFE0
-#define GBLUE			 	0X07FF
+#define GRED 		0XFFE0
+#define GBLUE		0X07FF
 #define RED         0xF800
 #define MAGENTA     0xF81F
 #define GREEN       0x07E0
 #define CYAN        0x7FFF
 #define YELLOW      0xFFE0
-#define BROWN 			0XBC40 //棕色
-#define BRRED 			0XFC07 //棕红色
-#define GRAY  			0X8430 //灰色
+#define BROWN 	    0XBC40 //棕色
+#define BRRED 		0XFC07 //棕红色
+#define GRAY  		0X8430 //灰色
 //GUI颜色
 
 #define DARKBLUE      	 0X01CF	//深蓝色
@@ -71,8 +71,8 @@ extern uint16_t  BACK_COLOR; //背景颜色.默认为白色
 //以上三色为PANEL的颜色 
  
 #define LIGHTGREEN     	0X841F //浅绿色
-#define LIGHTGRAY     0XEF5B //浅灰色(PANNEL)
-#define LGRAY 			 		0XC618 //浅灰色(PANNEL),窗体背景色
+#define LIGHTGRAY       0XEF5B //浅灰色(PANNEL)
+#define LGRAY 			 0XC618 //浅灰色(PANNEL),窗体背景色
 
 #define LGRAYBLUE      	0XA651 //浅灰蓝色(中间层颜色)
 #define LBBLUE          0X2B12 //浅棕蓝色(选择条目的反色)
