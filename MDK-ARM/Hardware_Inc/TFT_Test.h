068304785a974ecc5b9b648e5715929640715c25
Sanqiuzhiji <2772179523@qq.com>
2024-06-16 22:53:47 +0800

    12

---
+++ MDK-ARM/Hardware_Inc/TFT_Test.h
@@ -0,0 +1,19 @@
+#ifndef __TFT_TEST_H
+#define __TFT_TEST_H
+
+#include "main.h"
+
+void DrawTestPage(uint8_t *str);
+void Test_Color(void);
+void Test_FillRec(void);
+void Test_Circle(void);
+void Test_Triangle(void);
+void English_Font_test(void);
+void Chinese_Font_test(void);
+void Pic_test(void);
+// void Load_Drow_Dialog(void);
+void Touch_Test(void);
+void main_test(void);
+void Rotate_Test(void);
+
+#endif
